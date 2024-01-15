85c46424bc2c06d8ccbf2f854c296a997d74a353
Jchips <20979096+Jchips@users.noreply.github.com>
2024-01-14 16:13:24 -0800

    got code working and wrote tests

--- __tests__/v1.test.js
+++ __tests__/v1.test.js
@@ -3,25 +3,24 @@
 const { server } = require('../src/server');
 const supertest = require('supertest');
 const { db, food } = require('../src/models');
-const { users } = require('../src/auth/models');
-// const { expect } = require('@jest/globals');
+// const { users } = require('../src/auth/models');
 const request = supertest(server);
 
-let person;
-let person2;
+// let person;
+// let person2;
 
 beforeAll(async () => {
   await db.sync();
-  person = await users.create({
-    username: 'fox',
-    password: 'password123',
-    role: 'admin',
-  });
-  person2 = await users.create({
-    username: 'goat',
-    password: 'password123',
-    role: 'user',
-  });
+  // person = await users.create({
+  //   username: 'fox',
+  //   password: 'password123',
+  //   role: 'admin',
+  // });
+  // person2 = await users.create({
+  //   username: 'goat',
+  //   password: 'password123',
+  //   role: 'user',
+  // });
   await food.create({
     name: 'pear',
     calories: 7,
@@ -39,21 +38,14 @@ afterAll(async () => {
 });
 
 describe('Access control', () => {
-  test('allows read access', async () => {
-    let response = await request.get('/api/v2/food').set('Authorization', `Bearer ${person.token}`);
-    let response2 = await request.get('/api/v2/food/1').set('Authorization', `Bearer ${person.token}`);
+  test('returns a list of food items or a single food item from given ID', async () => {
+    let response = await request.get('/api/v1/food');
+    let response2 = await request.get('/api/v1/food/1');
 
     expect(response.status).toEqual(200);
     expect(response.body.length).toEqual(2);
     expect(response2.status).toEqual(200);
     expect(response2.body.name).toEqual('pear');
-  });
-
-  test('does not allow a reader update access', async () => {
-    let response = await request.put('/api/v2/food/0').set('Authorization', `Bearer ${person2.token}`);
-
-    expect(response.status).toEqual(500);
-    expect(response.text).toEqual('{"status":500,"message":"Access Denied"}');
   });
 
   test('adds an item to the DB and returns an object with the added item', async () => {
@@ -62,7 +54,7 @@ describe('Access control', () => {
       calories: 100,
       type: 'protein',
     };
-    let response = await request.post('/api/v2/food').set('Authorization', `Bearer ${person.token}`).send(object);
+    let response = await request.post('/api/v1/food').send(object);
 
     expect(response.status).toBe(201);
     expect(response.body.name).toEqual('cheese');
@@ -74,15 +66,15 @@ describe('Access control', () => {
       calories: 90,
       type: 'protein',
     };
-    let response = await request.put('/api/v2/food/3').set('Authorization', `Bearer ${person.token}`).send(object);
+    let response = await request.put('/api/v1/food/3').send(object);
 
     expect(response.status).toBe(200);
     expect(response.body.calories).toEqual(90);
   });
 
   test('returns an empty object. Subsequent GET for the same ID should result in nothing found.', async () => {
-    let response = await request.delete('/api/v2/food/3').set('Authorization', `Bearer ${person.token}`);
-    let response2 = await request.get('/api/v2/food/3').set('Authorization', `Bearer ${person.token}`);
+    let response = await request.delete('/api/v1/food/3');
+    let response2 = await request.get('/api/v1/food/3');
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(1);
